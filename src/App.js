04da97bc885b8f3f3bcc911c9508b3639f3478f0
Ayan Ahmad Khan <ayyanahmad932@gmail.com>
2022-03-15 18:21:18 +0500

    Search Bar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import data from "./data.json";
 import Header from "./Header";
 import ToDoList from "./ToDoList";
 import ToDoForm from './ToDoForm';
+import TextField from "@mui/material/TextField";
+import search from './search';
 
 function App() {
   
@@ -12,7 +14,14 @@ function App() {
   const [userInput, setUserInput ] = useState('');
   const [startDate, setStartDate] = useState(new Date());
   const [pirority, setPirority ] = useState('Low');
+  const [inputText, setInputText] = useState("");
 
+  // const inputHandler = (e) => {
+  //   //convert input text to lower case
+  //   console.log(e.target.value)
+  //   let filtered = toDoList.filter.includes(e.target.value)
+  //   setToDoList(filtered);
+  // };
 
   const handleToggle = (id) => {
     let mapped = toDoList.map(task => {
@@ -20,6 +29,24 @@ function App() {
     });
     setToDoList(mapped);
   }
+
+  const handleSearch = (inputValue) =>{
+    let filtered = toDoList.filter.includes(inputValue);
+    setToDoList(filtered);
+  }
+
+  const handleSearch11 = ((search) =>{
+    if (search === '') {
+     // return el;
+    }
+    //return the item which contains the user input
+    else {
+      let filtered = toDoList.filter.includes(search)(task => {
+        return !task.complete;
+      });
+        return search.text.toLowerCase().includes(search)
+    }
+  })
 
   const handleFilter = () => {
     let filtered = toDoList.filter(task => {
@@ -45,18 +72,27 @@ function App() {
         <div class="col-lg-4 col-md-3 col-sm-2"></div>
 
         <div class="col-lg-4 col-md-6 col-sm-8">
-          <ToDoForm addTask={addTask} userInput={userInput} setUserInput={setUserInput} startDate={startDate} setStartDate={setStartDate} pirority={pirority} setPirority={setPirority}/>
+          <ToDoForm addTask={addTask} userInput={userInput} setUserInput={setUserInput} 
+          startDate={startDate} setStartDate={setStartDate} pirority={pirority} setPirority={setPirority}/>
         </div>
 
         <div class="col-lg-4 col-md-3 col-sm-2"></div>
       </div>
-
+<div style={{marginBlock: "20px"}}>
+  <div class="container" style={{display: 'inline', padding: '2px 0px',  border: '2px solid black'}}>
+    <input placeholder=" Search . . ."  value={inputText} onChange={(e)=>setInputText(e.target.value)} style={{border: "none", outline: "none"
+    }}></input>
+    <div style={{alignContent: 'center',backgroundColor: "#0091C0", display: 'inline', padding: "2px", paddingLeft: "8px", paddingRight: "8px", borderColor:"black", borderLeft: "2px solid black"}}>
+      <i class="fa fa-search" ></i>
+      </div>
+    </div>
+  </div>
       
       <div class="row">
         <div class="col-sm-2"></div>
 
         <div class="col-sm-8">
-          <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+          <ToDoList toDoList={toDoList} search={inputText} handleToggle={handleToggle} handleFilter={handleFilter}/>
         </div>
 
         <div class="col-sm-2"></div>
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ToDo from './ToDo';
 
-const ToDoList = ({toDoList, handleToggle, handleFilter}) => {
+const ToDoList = ({toDoList, handleToggle, handleFilter,search}) => {
     return (
         <div>
             
@@ -17,9 +17,12 @@ const ToDoList = ({toDoList, handleToggle, handleFilter}) => {
             </table>
             
             {toDoList.map(todo => {
-                return (
-                    <ToDo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter}/>
-                )
+                if(todo.task.toLowerCase().indexOf(search.toLowerCase())>-1){
+
+                    return (
+                        <ToDo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter}/>
+                    )
+                }
             })}
             <button style={{margin: '20px'}} onClick={handleFilter}>Clear Completed</button>
         </div>
